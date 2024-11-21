8288d964a658e5d9c614b4a3e647b50ebefdf801
bxviet515 <vietsdk6@gmail.com>
2024-11-21 18:34:00 +0700

    dispatch command, viet api them sach

---
+++ bookservice/src/main/java/com/xuanviet/bookservice/command/event/BookEventsHandler.java
@@ -0,0 +1,20 @@
+package com.xuanviet.bookservice.command.event;
+
+import com.xuanviet.bookservice.command.data.Book;
+import com.xuanviet.bookservice.command.data.BookRepository;
+import org.axonframework.eventhandling.EventHandler;
+import org.springframework.beans.BeanUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+@Component
+public class BookEventsHandler {
+    @Autowired
+    private BookRepository bookRepository;
+    @EventHandler
+    public void on(BookCreatedEvent event){
+        Book book = new Book();
+        BeanUtils.copyProperties(event, book);
+        bookRepository.save(book);
+    }
+}

3dc80b151378d7ddbc1f47b54784d805e8444149
bxviet515 <vietsdk6@gmail.com>
2024-11-29 01:06:44 +0700

    viet api addEmployee

---
+++ employeeservice/src/main/java/com/xuanviet/employeeservice/command/aggregate/EmployeeAggregate.java
@@ -0,0 +1,40 @@
+package com.xuanviet.employeeservice.command.aggregate;
+
+import com.xuanviet.employeeservice.command.command.CreateEmployeeCommand;
+import com.xuanviet.employeeservice.command.event.EmployeeCreatedEvent;
+import lombok.NoArgsConstructor;
+import org.axonframework.commandhandling.CommandHandler;
+import org.axonframework.eventsourcing.EventSourcingHandler;
+import org.axonframework.modelling.command.AggregateIdentifier;
+import org.axonframework.modelling.command.AggregateLifecycle;
+import org.axonframework.spring.stereotype.Aggregate;
+import org.springframework.beans.BeanUtils;
+
+@Aggregate
+@NoArgsConstructor
+public class EmployeeAggregate {
+    @AggregateIdentifier
+    private String id;
+    private String firstName;
+    private String lastName;
+    private String kin;
+    private  Boolean isDisciplined;
+
+    @CommandHandler
+    public EmployeeAggregate(CreateEmployeeCommand command){
+        EmployeeCreatedEvent event = new EmployeeCreatedEvent();
+        BeanUtils.copyProperties(command, event);
+        AggregateLifecycle.apply(event);
+    }
+
+    @EventSourcingHandler
+    public void on(EmployeeCreatedEvent event){
+        this.id = event.getId();
+        this.firstName = event.getFirstName();
+        this.lastName = event.getLastName();
+        this.kin = event.getKin();
+        this.isDisciplined = event.getIsDisciplined();
+    }
+
+
+}
---
+++ employeeservice/src/main/java/com/xuanviet/employeeservice/command/model/CreateEmployeeModel.java
@@ -0,0 +1,21 @@
+package com.xuanviet.employeeservice.command.model;
+
+import jakarta.validation.constraints.NotBlank;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class CreateEmployeeModel {
+    @NotBlank(message = "First name is mandatory")
+    private String firstName;
+    @NotBlank(message = "Last name is mandatory")
+    private String lastName;
+    @NotBlank(message = "Kin is mandatory")
+    private String kin;
+
+}

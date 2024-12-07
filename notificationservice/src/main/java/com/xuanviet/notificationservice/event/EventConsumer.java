0eb5c5f8c914579364768c9290dc705ae6dc7f95
bxviet515 <vietsdk6@gmail.com>
2024-12-07 10:58:30 +0700

    ung dung retries va dead letter queue

--- notificationservice/src/main/java/com/xuanviet/notificationservice/event/EventConsumer.java
+++ notificationservice/src/main/java/com/xuanviet/notificationservice/event/EventConsumer.java
@@ -1,14 +1,32 @@
 package com.xuanviet.notificationservice.event;
 
 import lombok.extern.slf4j.Slf4j;
+import org.apache.kafka.common.errors.RetriableException;
+import org.springframework.kafka.annotation.DltHandler;
 import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.kafka.annotation.RetryableTopic;
+import org.springframework.kafka.retrytopic.DltStrategy;
+import org.springframework.messaging.handler.annotation.Payload;
+import org.springframework.retry.annotation.Backoff;
 import org.springframework.stereotype.Component;
 
 @Component
 @Slf4j
 public class EventConsumer {
+    @RetryableTopic(
+            attempts = "4",
+            backoff = @Backoff(delay = 1000, multiplier = 2),
+            autoCreateTopics = "true",
+            dltStrategy = DltStrategy.FAIL_ON_ERROR,
+            include = {RetriableException.class, RuntimeException.class}
+    )
+
     @KafkaListener(topics = "test", containerFactory = "kafkaListenerContainerFactory")
     public void listen(String message){
         log.info("Received message: "+message);
+    }
+    @DltHandler
+    void processDltMessage(@Payload String message){
+        log.info("DLT receive message: "+message);
     }
 }
